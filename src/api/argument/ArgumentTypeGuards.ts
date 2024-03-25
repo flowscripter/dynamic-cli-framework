f61843d8b767f0f3d81ccac83d342ae8ed5fb4a1
vectronic <hello.vectronic@gmail.com>
2024-03-25 20:03:57 +0000

    chore: fix merge

--- src/api/argument/ArgumentTypeGuards.ts
+++
@@ -1,8 +0,0 @@
-import Argument from "./Argument.ts";
-import ComplexOption from "./ComplexOption.ts";
-
-export function isComplexOption(
-  argument: Argument | ComplexOption,
-): argument is ComplexOption {
-  return (argument as ComplexOption).properties !== undefined;
-}
