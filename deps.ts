1ddd96a39ece0b7e05ff2f6961edcbd589e1260e
vectronic <vectronic@users.noreply.github.com>
2024-03-28 00:30:45 +0000

    chore: update dependencies

--- deps.ts
+++ deps.ts
@@ -1,12 +1,12 @@
-export * as log from "https://deno.land/std@0.220.1/log/mod.ts";
-export { sprintf } from "https://deno.land/std@0.220.1/fmt/printf.ts";
-export * as colors from "https://deno.land/std@0.220.1/fmt/colors.ts";
-export * as io from "https://deno.land/std@0.220.1/io/mod.ts";
+export * as log from "https://deno.land/std@0.221.0/log/mod.ts";
+export { sprintf } from "https://deno.land/std@0.221.0/fmt/printf.ts";
+export * as colors from "https://deno.land/std@0.221.0/fmt/colors.ts";
+export * as io from "https://deno.land/std@0.221.0/io/mod.ts";
 export { default as levenshtein } from "https://deno.land/x/levenshtein@v1.0.1/mod.ts";
-export * as path from "https://deno.land/std@0.220.1/path/mod.ts";
+export * as path from "https://deno.land/std@0.221.0/path/mod.ts";
 export { default as figlet_factory } from "https://x.nest.land/deno-figlet@0.0.5/deps/factory.js";
 export { default as figlet_serializer } from "https://x.nest.land/deno-figlet@0.0.5/deps/serializer.js";
 export { createEmphasize } from "npm:emphasize@7.0.0";
 export { default as jsonSyntaxDefinition } from "https://unpkg.com/@highlightjs/cdn-assets@11.9.0/es/languages/json.min.js";
 export type { LanguageFn as HighlightSyntax } from "https://esm.sh/highlight.js@11.9.0/";
-export { writeAll } from "https://deno.land/std@0.220.1/io/write_all.ts";
+export { writeAll } from "https://deno.land/std@0.221.0/io/write_all.ts";
