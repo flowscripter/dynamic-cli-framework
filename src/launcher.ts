3608b0ebb4786e5149ba15c0c7749b194c90be2e
vectronic <hello.vectronic@gmail.com>
2025-03-13 18:11:40 +0000

    fix: fix launcher

--- src/launcher.ts
+++ src/launcher.ts
@@ -7,29 +7,16 @@ import type Command from "./api/command/Command.ts";
 import type RunResult from "./api/RunResult.ts";
 import type { ServiceProvider } from "./api/service/ServiceProvider.ts";
 
-function parseVersion(moduleUrl?: string): string | undefined {
-  if (moduleUrl === undefined) {
-    return undefined;
-  }
-
-  const v = moduleUrl.match(/@([^\/]+)[\/$]?/);
-  if (v === null) {
-    return undefined;
-  }
-  return v[1];
-}
-
 /**
  * Launch a {@link DefaultRuntimeCLI} with a single specified {@link SubCommand} instance.
  *
  * @param command the {@link SubCommand} to add to the CLI.
  * @param description optional description of the CLI.
  * @param name optional name of the CLI. If not provided, `process.execPath` will be used in an attempt to derive the name.
- * @param version optional version of the CLI. If not provided the optionally provided {@link callingModuleUrl} will be used in an attempt to derive the version. If not possible the default is `N/A`.
+ * @param version optional version of the CLI.
  * @param envVarsEnabled optionally support checking env variables for default argument values.
  * @param configEnabled optionally enable configuration file support for default argument values.
  * @param keyValueServiceEnabled optionally provide a {@link KeyValueService} implementation: `configEnabled` must be true in this case
- * @param callingModuleUrl optional URL of the calling module, can be used in an attempt to derive the version of the CLI if {@link version} is not provided. If not possible the default is `N/A`.
  * @param serviceProviders optional array of {@link ServiceProvider} instances to add to the CLI.
  */
 export async function launchSingleCommandCLI(
@@ -40,7 +27,6 @@ export async function launchSingleCommandCLI(
   envVarsEnabled = false,
   configEnabled = false,
   keyValueServiceEnabled = false,
-  callingModuleUrl?: string,
   serviceProviders?: ReadonlyArray<ServiceProvider>,
 ): Promise<RunResult> {
   if (!name) {
@@ -49,7 +35,7 @@ export async function launchSingleCommandCLI(
   const cliConfig: CLIConfig = {
     description,
     name,
-    version: version || parseVersion(callingModuleUrl) || "N/A",
+    version: version || "N/A",
   };
 
   if (!configEnabled && keyValueServiceEnabled) {
@@ -79,11 +65,10 @@ export async function launchSingleCommandCLI(
  * @param commands the {@link Command} instances to add to the CLI.
  * @param description optional description of the CLI.
  * @param name optional name of the CLI. If not provided, `process.execPath` will be used in an attempt to derive the name.
- * @param version optional version of the CLI. If not provided the optionally provided {@link callingModuleUrl} will be used in an attempt to derive the version. If not possible the default is `N/A`.
+ * @param version optional version of the CLI.
  * @param envVarsEnabled optionally support checking env variables for default argument values.
  * @param configEnabled optionally enable configuration file support for default argument values.
  * @param keyValueServiceEnabled optionally provide a {@link KeyValueService} implementation: `configEnabled` must be true in this case
- * @param callingModuleUrl optional URL of the calling module, can be used in an attempt to derive the version of the CLI if {@link version} is not provided. If not possible the default is `N/A`.
  * @param serviceProviders optional array of {@link ServiceProvider} instances to add to the CLI.
  */
 export async function launchMultiCommandCLI(
@@ -94,7 +79,6 @@ export async function launchMultiCommandCLI(
   envVarsEnabled = false,
   configEnabled = false,
   keyValueServiceEnabled = false,
-  callingModuleUrl?: string,
   serviceProviders?: ReadonlyArray<ServiceProvider>,
 ): Promise<RunResult> {
   if (!name) {
@@ -103,7 +87,7 @@ export async function launchMultiCommandCLI(
   const cliConfig: CLIConfig = {
     description,
     name,
-    version: version || parseVersion(callingModuleUrl) || "N/A",
+    version: version || "N/A",
   };
   if (!configEnabled && keyValueServiceEnabled) {
     throw new Error(
